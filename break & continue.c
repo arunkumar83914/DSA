41447463b16261e241b332fc6e0e09365e1deba6
arunpsit <arun.psit@outlook.com>
2026-04-21 11:08:55 +0530

    Break & Continue

---
+++ break & continue.c
@@ -0,0 +1,77 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=1;i<=5;i++)
+//     {
+//         if(i==3)
+//         break;
+//         printf("%d\t",i);
+//     }
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=1;i<=5;i++)
+//     {
+//         if(i==3)
+//         continue;
+//         printf("%d\t",i);
+//     }
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,i,g;
+//     printf("enter two no");
+//     scanf("%d%d",&a,&b);
+//     for(i=1;i<=a&&i<b;i++)
+//     if(a%i==0&&b%i==0)
+//     g=i;
+//     printf("The GCD is = %d",g);
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,i,f=0;
+//     for(a=100;a<=500;a++)
+//     {
+//         for(i=1;i<=a;i++)
+//         if(a%i==0)
+//         f=f+1;
+//         if(f==2)
+//         printf("%d\t",a);
+//         f=0;
+//     }
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,t,f,b;
+//     for(a=1;a<=100;a++)
+//     {
+//         t=a;
+//         f=0;
+//         while(t>0)
+//         {
+//         b=t%10;
+//         f=f*10+b;
+//         t=t/10;
+//         }
+//         if(a==f)
+//         printf("%d\t",a);
+//     }
+// }
+
+
+
+
+
+
+
+

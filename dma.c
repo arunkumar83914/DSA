8e9454c8be5474306a2834b3b6b4ca7c363e0248
arunpsit <arun.psit@outlook.com>
2026-04-22 15:19:31 +0530

    DMA

---
+++ dma.c
@@ -0,0 +1,46 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n,i,s=0;
+//     printf("Enter the elements");
+//     scanf("%d",n);
+//     int *a;
+//     a=(int *) malloc (n*(sizeof(int)));
+//     for(i=0;i<n;i++)
+//     {
+//         printf("Enter a no");
+//         scanf("%d",a+i);
+//         s=s+*(a+i);
+//         }
+// //         printf("The sum is = %d",s)
+// #include<stdio.h>
+// int main()
+// {
+//     int *a;
+//     int n,i,s;
+//     printf("Enter the elements");
+//     scanf("%d",&n);
+//     a=(int a) calloc (n*sizeof(int));
+//     for(i=0;i<n;i++)
+//     {
+//         printf("Enter two no");
+//         scanf("%d",a+i);
+//         s=s+*(a+i);
+//         }
+//         printf("The sum is = %d",s);
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int *a;
+//     a = (int*) malloc(size of(int));
+//     realloc (a,5)
+//     a[1] = 10;
+//     a[2] = 20;
+//     a[3] = 30;
+//     a[4] = 40;
+//     printf("Third element =%d",a[3]);
+// }
+
+

6563329150835953284dfa4b01ce190e32019351
arunpsit <arun.psit@outlook.com>
2026-04-24 13:11:56 +0530

    Function

---
+++ function.c
@@ -0,0 +1,213 @@
+// #include<stdio.h>
+// void sum(void);
+// int main()
+// {
+//     sum();
+// }
+// void sum(void)
+// {
+//     int a,b,c;
+//     printf("enter two no");
+//     scanf("%d%d",&a,&b);
+//     c=a+b;
+//     printf("Sum is = %d",c);
+// }
+// #include<stdio.h>
+// void fact(void);
+// int main()
+// {
+//     fact();
+// }
+// void fact(void)
+// {
+//     int a,f=1,i;
+//     printf("enter a no");
+//     scanf("%d",&a);
+//     for(i=1;i<=a;i++)
+//     f=f*i;
+//     printf("The factorial no is = %d",f);
+// }
+// #include<stdio.h>
+// void prime(void);
+// int main()
+// {
+//     prime();
+// }
+// void prime(void)
+// {
+//     int a,f=0,i;
+//     printf("enter a no");
+//     scanf("%d",&a);
+//     for(i=1;i<=a;i++)
+//     if(a%i==0)
+//     f=f+1;
+//     if(f==2)
+//     printf("Prime");
+//     else
+//     printf("Not Prime");
+// }
+// #include<stdio.h>
+// void sum(int,int);
+// int main()
+// {
+//     int a,b;
+//     printf("enter two no");
+//     scanf("%d%d",&a,&b);
+//     sum (a,b);
+// }
+// void sum(int c,int d)
+// {
+//     int l;
+//     l=c+d;
+//     printf("The sum is = %d",l);
+// }
+// #include<stdio.h>
+// void fact(int);
+// int main()
+// {
+//     int a;
+//     printf("enter a no");
+//     scanf("%d",&a);
+//     fact (a);
+// }
+// void fact(int b)
+// {
+//     int i,f=1;
+//     for(i=1;i<=b;i++)
+//     f=f*i;
+//     printf("Factorial is  = %d",f);
+// }
+// #include<stdio.h>
+// void prime(int);
+// int main()
+// {
+//     int a;
+//     printf("enter a no");
+//     scanf("%d",&a);
+//     prime (a);
+// }
+// void prime(int b)
+// {
+//     int i,f=0;
+//     for(i=1;i<=b;i++)
+//     if(b%i==0)
+//     f=f+1;
+//     if(f==2)
+//     printf("Prime");
+//     else
+//     printf("Not Prime");
+// }
+// #include<stdio.h>
+// int sum(void);
+// int main()
+// {
+//     int r;
+//     r=sum();
+//     printf("The sum is = %d",r);
+// }
+// int sum(void)
+// {
+//     int a,b,c;
+//     printf("enter two no");
+//     scanf("%d%d",&a,&b);
+//     c=a+b;
+//     return 0;
+// }
+// #include<stdio.h>
+// int fact(void);
+// int main()
+// {
+//     int r;
+//     r=fact();
+//     printf("The factorial is = %d",r);
+// }
+// int fact(void)
+// {
+//     int a,i,f=1;
+//     printf("enter a no");
+//     for(i<5;i<=a;i++)
+//     f=f*i;
+//     return (f);
+// }
+// #include<stdio.h>
+// int prime(void);
+// int main()
+// {
+//     int r;
+//     r=prime();
+//     if(r==2)
+//     printf("enter a no");
+//     else
+//     printf("Not Prime");
+// }
+// int prime(void)
+// {
+//     int a,i,f=0;
+//     printf("enetr a no");
+//     for(i=1;i<=a;i++)
+//     if(a%i==0)
+//     f=f+1;
+//     return (f);
+// }
+// #include<stdio.h>
+// int sum(int,int);
+// int main()
+// {
+//    int a,b,r;
+//    printf("enter two no");
+//    scanf("%d%d",&a,&b);
+//    sum (a,b);
+//    printf("The sum is = %d",r);
+// }
+// int sum(int c,int d)
+// {
+//     int i;
+//     i=c+d;
+//     return (i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

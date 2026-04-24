55b339d9286514d18add0f75b9b092df58bdee7f
arunpsit <arun.psit@outlook.com>
2026-04-24 13:12:52 +0530

    1st

---
+++ First.c
@@ -0,0 +1,53 @@
+// #include<stdio.h>
+// int main()
+// {
+// printf("Hello World");
+// }
+// #include<stdio.h>
+// int main()
+// {
+// int a,b,c;
+// a=10;
+// b=20;
+// c=a+b;
+// printf("The sum is = %d",c);
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,c;
+//     printf("Enter two no");
+//     scanf("%d%d",&a,&b);
+//     c=a*b;
+//     printf("The multiply is = %d",c);
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,c;
+//     printf("Enter two no");
+//     scanf("%d%d",&a,&b);
+//     c=a;
+//     a=b;
+//     b=c;
+//     printf("After Swapping a=%d,b=%d",a,b);
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     printf("Enter two no");
+//     scanf("%d%d",&a,&b);
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
+//     printf("After Swapping a=%d,b=%d",a,b);
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     char a;
+//     printf("Enter a character");
+//     scanf("%c",&a);
+//     printf("The ASCII code is = %d",a);
+// }

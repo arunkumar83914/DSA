7c675d4a849dd8e9c4244019b9e83ead8f6c8d98
arunpsit <arun.psit@outlook.com>
2026-04-25 18:57:45 +0530

    Structure

---
+++ structure.c
@@ -0,0 +1,208 @@
+// #include<stdio.h>
+// struct student
+// {
+//     int rollno;
+//     char name[20];
+//     char city[20];
+// };
+
+// #include<stdio.h>
+// struct student
+// {
+//     int rollno;
+//     char name[20];
+//     char city[20];
+// }a;
+
+// #include<stdio.h>
+// struct student
+// {
+//     int rollno;
+//     char name[20];
+//     char city[20];
+// }a;
+// int main()
+// {
+//  printf("Enter the rollno,name,city of student");
+//  scanf("%d%s%s",&a.rollno,a.name,a.city);
+//  printf("The details of student is \n");
+//  printf("Rollno=%d",a.rollno);
+//  printf("\n Name=%s",a.name);
+//  printf("\n City=&s,a.city");
+// }
+
+// #include<stdio.h>
+// struct student
+// {
+//     char name[20];
+//     int age;
+//     float per;
+//     char city[20];
+// }a[20];
+// int main()
+// {
+//     int i;
+//     for(i=0;i<20;i++)
+//     {
+//  printf("Enter the name,age,percentage,city");
+//  scanf("%s%d%f%s",a[i].name,&a[i].age,&a[i].per,a[i].city);
+//     }
+//  printf("The students details are \n");
+//  printf("Name\t\tAge\t\tpercentage\t\tCity\n");
+//  for(i=0;i<20;i++)
+//  printf("%s\t\t%d\t\t%f\t\t%s\n",a[i].name,a[i].age,a[i].per,a[i].city);
+// }
+
+// #include<stdio.h>
+// struct student
+// {
+//     char name[20];
+//     int rollno;
+//     float per;
+// }a[20];
+// int main()
+// {
+//     int i;
+//     for(i=0;i<20;i++)
+//     {
+//  printf("enter the name,percentage,rollno");
+//  scanf("%s%d%f",&a[i].name,&a[i].rollno,&a[i].per);
+//     }
+//  printf("The students details are \n");
+//  printf("Name\t\trollno\t\tpercentage\n");
+//  for(i=0;i<20;i++)
+//  {
+//     if(a[i].per>60)
+//  printf("%s\t\t%d\t\t%f\n",&a[i].name,&a[i].rollno,&a[i].per);
+// }
+
+// #include<stdio.h>
+// struct student
+// {
+//     char name[20];
+//     int rollno;
+//     float per;
+// }a[20];
+// int main()
+// {
+//     int i;
+//     for(i=0;i<20;i++)
+//     {
+//  printf("enter the name,percentage,rollno");
+//  scanf("%s%d%f",&a[i].name,&a[i].rollno,&a[i].per);
+//     }
+//  printf("The students details are \n");
+//  printf("Name\t\trollno\t\tpercentage\n");
+//  for(i=0;i<20;i++)
+//  {
+//     if(a[i].per>60)
+//  printf("%s\t\t%d\t\t%f\n",&a[i].name,&a[i].rollno,&a[i].per);
+// }
+// }
+
+// #include<stdio.h>
+// struct student
+// {
+//     char name[20];
+//     int rollno;
+//     float per;
+// }a[20];
+// int main()
+// {
+//     int i;
+//     for(i=0;i<20;i++)
+//     {
+//  printf("enter the name,rollno,percentage");
+//  scanf("%s%d%f",&a[i].name,&a[i].rollno,&a[i].percentage);
+//     }
+//  printf("The students details are\n");
+//  printf("Name\t\trollno\t\tpercentage\n");
+//  for(i=0;i<20;i++)
+//  {
+//  if(a[i].name[0]=='s'||a[i].name[0]='s')
+//  printf("%s\t\t%d\t\t%f\n",a[i].name,a[i].rollno,a[i].per);
+// }
+// }
+
+// #include<stdio.h>
+// struct abc
+// {
+//     int a;
+//     int b;
+// };
+// main()
+// {
+//     struct abc * r={10,20};
+//     printf("%d%d",r=a,r=b);
+// }
+
+// #include<stdio.h>
+// struct abc
+// {
+// int a;
+// int b;
+// };
+// int main()
+// {
+//     struct abc r={10,20};
+//     display(r);
+// }
+// display(struct abc t)
+// {
+//     printf("%d%d",t.a,t.b);
+// }
+
+// #include<stdio.h>
+// struct abc
+// {
+// int a;
+// int b;
+// };
+// struct student
+// {
+// char name[20] ;
+// struct abc r;
+// };
+// int main()
+// {
+// struct student t={"Ram",10,20};
+// printf("%s%d%d",t.name,t.r.a,t.r.b);
+// }
+
+// #include<stdio.h>
+// struct student
+// {
+//     char name[20];
+//     int rollno;
+//     float per;  
+// }a[100];
+// int main()
+// {
+//     int i,j;
+//     for(i=0;i<100;i++)
+//     printf("enter the name,rollno,percentage");
+//     for(i=0;i<100;i++)
+//     scanf("%s%d%f",a[i].name,&a[i].rollno,&a[i].per);
+//     for(i=0;i<100;i++)
+//     for(j=0;j<100-i-1;j++)
+//     if(a[j].per,a[j+i].per)
+//     {
+//         struct student t;
+//         t=a[j];
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

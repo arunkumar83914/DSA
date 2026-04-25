eac5cd8c6b217516d10c2b55e3bc39edf6eb6a6f
arunpsit <arun.psit@outlook.com>
2026-04-25 18:52:43 +0530

    Harry

---
+++ harry.c
@@ -0,0 +1,60 @@
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Arun Kumar");
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int age;
+//     printf("Enter your age\n");
+//     scanf("%d",&age);
+//     printf("You have entered %d as your age\n",age);
+//     if (age>=18)
+//     {
+//     printf("You can vote!");
+//     }
+//     else
+//     {
+//     printf("You can not vote!");
+//     }
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int num,index=0;
+//     printf("enter a no");
+//     scanf("%d",&num);
+//     do
+//     {
+//         printf("%d",index);
+//         index=index+1;
+//     }
+//     while(index<num);
+//     return 0;
+//     }
+// #include<stdio.h>
+// int main()
+// {
+//     int i=0;
+//     while(i<54)
+//     {
+//         printf("%d",i);
+//         i=i+1;
+//     }
+//     return 0;
+//     }
+// #include<stdio.h>
+// int main()
+// {
+// int i,j=0;
+// for(i=0;i<5;i++,j++)
+// {
+// printf("%d%d\n",i,j);
+// }
+// }
+
+
+

df427a796e6fda5e84c69c9b622c0f4e1306a6fb
arunpsit <arun.psit@outlook.com>
2026-04-25 18:55:41 +0530

    Loop

---
+++ Loop.c
@@ -0,0 +1,30 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=1;i<=5;i++)
+//     printf("\nArun");
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=2;i<=20;i=i+2)
+//     printf("\n%d",i);
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int a,i;
+//     printf("enter a no");
+//     scanf("%d",&a);
+//     for(i=1;i<=10;i++)
+//     printf("\n%d",a*i);
+// }
+// #include <stdio.h>
+// int main(){
+// int i , s = 0;
+// for(i = 2;i<100;i+=2)
+//     s = s+i;
+// printf("print:%d",s);
+// }
